6d39ea5c1bcf64a2b19efee253d196b04259117d
Pau Duran <alu2016340@fffotfffffff.local>
2018-11-14 15:28:57 +0100

    Initial Commit

---
+++ Griffins and Gargoyles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Griffins and Gargoyles
+//
+//  Created by Pau Duran on 14/11/2018.
+//  Copyright © 2018 Pau Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Griffins and GargoylesTests/Griffins_and_GargoylesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Griffins_and_GargoylesTests.swift
+//  Griffins and GargoylesTests
+//
+//  Created by Pau Duran on 14/11/2018.
+//  Copyright © 2018 Pau Duran. All rights reserved.
+//
+
+import XCTest
+@testable import Griffins_and_Gargoyles
+
+class Griffins_and_GargoylesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
